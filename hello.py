90d226e5995aec2991a9cb4d27b58d24e587ab91
Orlando Ramirez <ramirez.orlando17@gmail.com>
2021-01-24 15:16:05 -0400

    now the moment works needs the internet to do the job

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, make_response, render_template
+from datetime import datetime
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
@@ -8,7 +9,8 @@ moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', 
+                            current_time = datetime.utcnow())
 
 
 
