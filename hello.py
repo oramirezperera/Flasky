34af1706820328459505ad7b3fd9bdc2b5e73e3a
Orlando Ramirez <ramirez.orlando17@gmail.com>
2021-01-24 00:44:41 -0400

    created the gitignore file and playing around with the hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return f'<h1>Bad Request</h1>', 400
 
 
 @app.route('/user/<name>')
