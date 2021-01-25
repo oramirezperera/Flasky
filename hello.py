3fbc6fbe07974087ec2da7f382affce8f3021492
Orlando Ramirez <ramirez.orlando17@gmail.com>
2021-01-24 22:59:16 -0400

    changes to the hello.py to add a database to the application qith SQLAlchemy and using SQLite

--- hello.py
+++ hello.py
@@ -1,15 +1,21 @@
-from flask import Flask, request, make_response, render_template, session, redirect, url_for
+from flask import Flask, request, make_response, render_template, session, redirect, url_for, flash
 from datetime import datetime
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+import os
+from flask_sqlalchemy import SQLAlchemy
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:///' + os.path.join(basedir, 'data.sqlite'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bootstrap = Bootstrap(app)
-moment = Moment(app)
+
+db = SQLAlchemy(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
@@ -19,6 +25,9 @@ class NameForm(FlaskForm):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name')
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
