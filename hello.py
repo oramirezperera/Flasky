47b988c496e3be5866d9c120130b007b809f78f7
Orlando Ramirez <ramirez.orlando17@gmail.com>
2021-01-24 21:53:27 -0400

    modifications to index and to hello.py it's safer now, the name goes through session and not from a variable, now name is encrypted

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, make_response, render_template
+from flask import Flask, request, make_response, render_template, session, redirect, url_for
 from datetime import datetime
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import stringField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -15,10 +15,13 @@ class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html', 
-                            current_time = datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 
